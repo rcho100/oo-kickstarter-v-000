8afe055b4ace53e0a47cd85c77a57f1825630c6c
Richard Cho <richardcho100@gmail.com>
2018-05-18 03:00:19 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,11 +2,11 @@ class Backer
   attr_accessor :name, :backed_projects
   def initialize(name)
     @name = name
-    @backed_projects
+    @backed_projects = []
   end
 
   def back_project(project)
-    
+    @back_projects << project
   end
 
 
